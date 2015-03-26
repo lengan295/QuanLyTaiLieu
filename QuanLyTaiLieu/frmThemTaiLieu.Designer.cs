a9cb45893c8302cc837ff72fbed5649e5753ad48
lengan93 <lengan93ha@gmail.com>
2015-03-26 10:03:02 +0700

    Create Structure
    Tạo cấu trúc

--- QuanLyTaiLieu/frmThemTaiLieu.Designer.cs
+++ QuanLyTaiLieu/frmThemTaiLieu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuanLyTaiLieu
 {
-    partial class Form1
+    partial class frmThemTaiLieu
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "frmThemThuCong";
         }
 
         #endregion
     }
 }
-
--- QuanLyTaiLieu/frmThemTaiLieu.cs
+++ QuanLyTaiLieu/frmThemTaiLieu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyTaiLieu
 {
-    public partial class Form1 : Form
+    public partial class frmThemTaiLieu : Form
     {
-        public Form1()
+        public frmThemTaiLieu()
         {
             InitializeComponent();
         }
