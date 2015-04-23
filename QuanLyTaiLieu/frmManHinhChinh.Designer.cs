644bd218ca33797ad8975f9c3ce79a6d90d49536
lengan93 <lengan93ha@gmail.com>
2015-04-23 20:40:31 +0700

    add icon
    them icon vo man hinh chinh

--- QuanLyTaiLieu/frmManHinhChinh.Designer.cs
+++ QuanLyTaiLieu/frmManHinhChinh.Designer.cs
@@ -39,28 +39,41 @@
             this.button5 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 73);
+            this.treeView1.Location = new System.Drawing.Point(12, 93);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(157, 363);
+            this.treeView1.Size = new System.Drawing.Size(157, 409);
             this.treeView1.TabIndex = 0;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(807, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(872, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(175, 73);
+            this.listView1.Location = new System.Drawing.Point(175, 93);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(620, 249);
+            this.listView1.Size = new System.Drawing.Size(685, 279);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -68,9 +81,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(175, 328);
+            this.richTextBox1.Location = new System.Drawing.Point(175, 394);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(620, 108);
+            this.richTextBox1.Size = new System.Drawing.Size(685, 108);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
@@ -78,50 +91,50 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 27);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(57, 40);
+            this.button1.Size = new System.Drawing.Size(61, 60);
             this.button1.TabIndex = 4;
             this.button1.Text = "Thêm";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(75, 27);
+            this.button2.Location = new System.Drawing.Point(89, 27);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(57, 40);
+            this.button2.Size = new System.Drawing.Size(62, 60);
             this.button2.TabIndex = 4;
             this.button2.Text = "Xóa";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(138, 27);
+            this.button3.Location = new System.Drawing.Point(167, 27);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(57, 40);
+            this.button3.Size = new System.Drawing.Size(60, 60);
             this.button3.TabIndex = 4;
             this.button3.Text = "Cập Nhật";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(201, 27);
+            this.button4.Location = new System.Drawing.Point(243, 27);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(57, 40);
+            this.button4.Size = new System.Drawing.Size(64, 60);
             this.button4.TabIndex = 4;
             this.button4.Text = "Trích Dẫn";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(264, 27);
+            this.button5.Location = new System.Drawing.Point(323, 27);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(57, 40);
+            this.button5.Size = new System.Drawing.Size(72, 60);
             this.button5.TabIndex = 4;
             this.button5.Text = "Mở Tài Liệu";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(484, 37);
+            this.textBox1.Location = new System.Drawing.Point(510, 47);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(158, 20);
             this.textBox1.TabIndex = 5;
@@ -129,16 +142,75 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(666, 37);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tác giả",
+            "Tiêu đề",
+            "Năm",
+            "Tóm tắt",
+            "Ghi chú"});
+            this.comboBox1.Location = new System.Drawing.Point(731, 47);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(129, 21);
             this.comboBox1.TabIndex = 6;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 505);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(872, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tóm tắt:";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(674, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tìm theo:";
             // 
             // frmManHinhChinh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 459);
+            this.ClientSize = new System.Drawing.Size(872, 527);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button5);
@@ -153,6 +225,8 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmManHinhChinh";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +245,13 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- QuanLyTaiLieu/frmManHinhChinh.cs
+++ QuanLyTaiLieu/frmManHinhChinh.cs
@@ -15,10 +15,32 @@ namespace QuanLyTaiLieu
         public frmManHinhChinh()
         {
             InitializeComponent();
+
+            button1.Image = global::QuanLyTaiLieu.Properties.Resources.add;
+            button1.ImageAlign = ContentAlignment.TopCenter;
+            button1.TextAlign = ContentAlignment.BottomCenter;
+
+            button2.Image = global::QuanLyTaiLieu.Properties.Resources.delete;
+            button2.ImageAlign = ContentAlignment.TopCenter;
+            button2.TextAlign = ContentAlignment.BottomCenter;
+
+            button3.Image = global::QuanLyTaiLieu.Properties.Resources.edi;
+            button3.ImageAlign = ContentAlignment.TopCenter;
+            button3.TextAlign = ContentAlignment.BottomCenter;
+
+            button4.Image = global::QuanLyTaiLieu.Properties.Resources.citation1;
+            button4.ImageAlign = ContentAlignment.TopCenter;
+            button4.TextAlign = ContentAlignment.BottomCenter;
+
+            button5.Image = global::QuanLyTaiLieu.Properties.Resources.Open;
+            button5.ImageAlign = ContentAlignment.TopCenter;
+            button5.TextAlign = ContentAlignment.BottomCenter;
+
             listView1.Columns.Add("Tác giả",150);
             listView1.Columns.Add("Tiêu đề",300);            
             listView1.Columns.Add("Năm",50);
 
+            //button1.Enabled = false;
 
             string[] arr = new string[4];
             ListViewItem itm;
