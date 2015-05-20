bd961f819dc7fce322aa158dba0a2bfa5c08497d
Kudo <ntnhonqt@gmail.com>
2015-05-20 14:05:12 +0700

    Revert "Merge branch 'master' of https://github.com/lengan93/QuanLyTaiLieu"
    This reverts commit fd41f3e20dd0612481b69b8a118f3e0be784f2de, reversing
    changes made to 1a71d6fffc21b39f6e371103e85810c63cbdb8ba.

---
+++ QuanLyTaiLieu/frmThemTaiLieu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTaiLieu
+{
+    public partial class frmThemTaiLieu : Form
+    {
+        public frmThemTaiLieu()
+        {
+            InitializeComponent();
+        }
+
+        private void btbOk_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chưa chọn danh mục tài liệu thuộc vê.");
+        }
+
+        private void cbbLoaiTL_SelectedIndexChanged(object sender, EventArgs e)
+        {   
+            int index = cbbLoaiTL.SelectedIndex;
+            frmThemTaiLieu_Load(sender, e);
+            if (index == 1)
+            {
+                label2.Visible = false;
+                txtTacgia.Visible = false;
+            }
+            if (index == 2)
+            {
+                frmThemTaiLieu_Load(sender, e);
+            }
+        }
+
+        private void frmThemTaiLieu_Load(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+       
+    }
+}
