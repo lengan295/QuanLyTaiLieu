b1a4423d0441c303eb9c0286b513ed2c4080db81
cukingpro <mr.cukingpro@gmail.com>
2015-05-21 09:20:32 +0700

    huy 1

--- QuanLyTaiLieu/frmTrichDanNhieu.Designer.cs
+++ QuanLyTaiLieu/frmTrichDanNhieu.Designer.cs
@@ -36,8 +36,8 @@
             this.button4 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // label1
@@ -97,6 +97,10 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "APA",
+            "Harvard",
+            "Chicago"});
             this.comboBox1.Location = new System.Drawing.Point(105, 6);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
@@ -111,29 +115,29 @@
             this.label3.TabIndex = 10;
             this.label3.Text = "Trích dẫn";
             // 
-            // richTextBox1
+            // listView1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(42, 63);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(430, 113);
-            this.richTextBox1.TabIndex = 11;
-            this.richTextBox1.Text = "";
+            this.listView1.Location = new System.Drawing.Point(39, 56);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(448, 135);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
-            // richTextBox2
+            // listView2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(42, 222);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(511, 172);
-            this.richTextBox2.TabIndex = 11;
-            this.richTextBox2.Text = "";
+            this.listView2.Location = new System.Drawing.Point(39, 211);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(531, 194);
+            this.listView2.TabIndex = 12;
+            this.listView2.UseCompatibleStateImageBehavior = false;
             // 
             // frmTrichDanNhieu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 446);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button4);
@@ -159,7 +163,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
     }
 }
